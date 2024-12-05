42b505ec3e65f85bdc1cd1db9bbdec87002972e7
Belinda Trotta <btrotta@users.noreply.github.com>
2024-12-05 19:34:54 +1100

    Day 5

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Advent of code 2024
+
+Notes on selected problems.
+
+## Day 5
+
+Note that the order for all possible pairs of pages is specified (i.e. the length of the order instructions 
+is $n$ choose 2 where $n$ is the number of unique pages in the updates). So we can use the order to build a comparator 
+for sorting.
---
+++ day05a.py
@@ -0,0 +1,28 @@
+from utilities import *
+from functools import cmp_to_key
+
+arr = parse_single_string()
+
+order = set()
+updates = []
+for a in arr:
+    if "|" in a:
+        order.add(tuple(int(x) for x in a.split("|")))
+    elif "," in a:
+        updates.append([int(x) for x in a.split(",")])
+
+
+def compare(a, b):
+    if (a, b) in order:
+        return -1
+    else:
+        return 1
+
+
+ans = 0
+for update in updates:
+    sorted_update = sorted(update, key=cmp_to_key(compare))
+    if sorted_update == update:
+        ans += update[len(update) // 2]
+
+print(ans)
